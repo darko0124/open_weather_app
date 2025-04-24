a1a6786d1a41ad9c40947f6aec8dd30977174d42
darko0124 <darko0124@gmail.com>
2025-04-24 20:37:19 +0300

    Working call

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 import requests
 import yaml
 
-with open("D:\Python_Projects\Github_Repos\open_weather_app\config.yaml", "r") as file:  #Change this to be more generic
+# Load config from YAML
+with open("D:\\Python_Projects\\Github_Repos\\open_weather_app\\config.yaml", "r") as file:  #Change this to be more generic
     config = yaml.safe_load(file)
-    
-API_KEY = config["openweathermap"]["api_key"]
-CITY_NAME = "London"
+
+api_config = config["openweathermap"]
+API_KEY = api_config["api_key"]
+LAT = api_config["lat"]
+LON = api_config["lon"]
+
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 params = {
-    "q": CITY_NAME,
+    "lat": LAT,
+    "lon": LON,
     "appid": API_KEY,
     "units": "metric"
 }
@@ -18,7 +23,8 @@ response = requests.get(BASE_URL, params=params)
 
 if response.status_code == 200:
     data = response.json()
-    print(f"Weather in {CITY_NAME}: {data['weather'][0]['description']}")
+    location = data.get("name", "Unknown Location")
+    print(f"Weather in {location}: {data['weather'][0]['description']}")
     print(f"Temperature: {data['main']['temp']}°C")
 else:
     print("Error:", response.status_code, response.json())
