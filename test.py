1eddd576c39cc2fa0f17d8b652641bf71aa480bd
darko0124 <darko0124@gmail.com>
2025-04-24 22:17:38 +0300

    Adding logic for many cities to get data for

--- test.py
+++ test.py
@@ -7,24 +7,25 @@ with open("D:\\Python_Projects\\Github_Repos\\open_weather_app\\config.yaml", "r
 
 api_config = config["openweathermap"]
 API_KEY = api_config["api_key"]
-LAT = api_config["lat"]
-LON = api_config["lon"]
+CITIES = api_config["cities"]
 
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
-params = {
-    "lat": LAT,
-    "lon": LON,
-    "appid": API_KEY,
-    "units": "metric"
-}
+for city in CITIES:
+    params = {
+        "lat": city["lat"],
+        "lon": city["lon"],
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    
+    response = requests.get(BASE_URL, params=params)
 
-response = requests.get(BASE_URL, params=params)
-
-if response.status_code == 200:
-    data = response.json()
-    location = data.get("name", "Unknown Location")
-    print(f"Weather in {location}: {data['weather'][0]['description']}")
-    print(f"Temperature: {data['main']['temp']}°C")
-else:
-    print("Error:", response.status_code, response.json())
+    if response.status_code == 200:
+        data = response.json()
+        location = data.get("name", city["name"])
+        print(f"Weather in {location}: {data['weather'][0]['description']}")
+        print(f"Temperature: {data['main']['temp']}°C\n")
+    else:
+        print(f"Error fetching weather for {city['name']} :",
+            response.status_code, response.json())
